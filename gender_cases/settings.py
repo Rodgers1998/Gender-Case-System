a33b417edf68d7058296bc314781e48258ccd83f
Rodgers1998 <companybells@gmai.com>
2024-10-22 16:36:31 +0300

    Update dependencies

--- gender_cases/settings.py
+++ gender_cases/settings.py
@@ -87,18 +87,18 @@ WSGI_APPLICATION = 'gender_cases.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# import dj_database_url
-
 # DATABASES = {
-#     'default': dj_database_url.config(default='postgres://localhost/mydb')
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost/mydb')
+}
 
 # DATABASES = {
 #     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
